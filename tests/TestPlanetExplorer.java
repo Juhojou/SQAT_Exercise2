28391ab2205783377c9eab91833d427c6da07992
somename <someemail>
2016-10-19 14:39:38 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -7,10 +7,14 @@ public class TestPlanetExplorer {
 
 	@Test
 	public void test_planetIsMade() throws PlanetExplorerException{
-		int xkoord = 100;
-		int ykoord = 100;
 		PlanetExplorer explorer = new PlanetExplorer(100, 100, null);
 		assertNotNull(explorer);
+	}
+	
+	@Test
+	public void test_explorerIsFacingNorth() throws PlanetExplorerException{
+		PlanetExplorer explorer = new PlanetExplorer(100, 100, null);
+		assertEquals("(0,0,N)", getStatus(explorer));
 	}
 	
 }
