6bfd7961a49fc2c946c6ef77d66acd74e076434f
somename <someemail>
2016-10-19 15:27:58 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -30,6 +30,7 @@ public class PlanetExplorer {
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
 		String[] commands = command.split("");
+		System.out.print(commands.length);
 		for (int j=0; j<commands.length; j++)
 		{
 			if (commands[j] == "r"){
