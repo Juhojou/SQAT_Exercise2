c8a890604bdf0b2697da3ab528a08b932cea714c
somename <someemail>
2016-10-19 15:22:46 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -60,4 +60,12 @@ public class TestPlanetExplorer {
 		explorer.executeCommand("b");
 		assertEquals("(10,2,N)", explorer.getStatus());
 	}
+	
+	@Test
+	public void test_explorerMoves_f_f_r_f_f_andLandsToStatus_2_2_E() throws PlanetExplorerException{
+		PlanetExplorer explorer = new PlanetExplorer(100, 100, null);
+		explorer.setStatus("(10,3,N)");
+		explorer.executeCommand("b");
+		assertEquals("(10,2,N)", explorer.getStatus());
+	}
 }
