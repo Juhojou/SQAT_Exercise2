84370df6a3d85d48eca89f391566f3d3947f0c6d
somename <someemail>
2016-10-19 14:49:56 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -4,6 +4,9 @@
 // Finish time:
 
 public class PlanetExplorer {
+	
+	private String status;
+	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
@@ -25,12 +28,23 @@ public class PlanetExplorer {
 		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
+		if (command == "r"){
+			 turnRight();
+		}
 		
 		return null;
 	}
 
-	public Object getStatus() {
+	private void turnRight() {
+		setStatus("(0,0,E");
+	}
+
+	private void setStatus(String newStatus) {
+		status = newStatus;
+	}
+
+	public String getStatus() {
 		// TODO Auto-generated method stub
-		return "(0,0,N)";
+		return status";
 	}
 }
