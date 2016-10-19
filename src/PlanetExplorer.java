a1e7ef6814133b985fbbc7bf5a546d3756dd1bc1
somename <someemail>
2016-10-19 15:47:53 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -28,36 +28,21 @@ public class PlanetExplorer {
 		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
-		if (command.length() == 1){
-			if (command == "r"){
+	
+		String[] commands = command.split(",");
+		for (int j=0; j<commands.length; j++)
+		{
+			if (commands[j] == "r"){
 				 turnRight();
 			}
-			if (command == "l"){
+			if (commands[j] == "l"){
 				turnLeft();
 			}
-			if (command == "f"){
+			if (commands[j] == "f"){
 				goForward();
 			}
-			if (command == "b"){
+			if (commands[j] == "b"){
 				goBackward();
-			}
-		}
-		else {
-			String[] commands = command.split(",");
-			for (int j=0; j<command.length(); j++)
-			{
-				if (commands[j] == "r"){
-					 turnRight();
-				}
-				if (commands[j] == "l"){
-					turnLeft();
-				}
-				if (commands[j] == "f"){
-					goForward();
-				}
-				if (commands[j] == "b"){
-					goBackward();
-				}
 			}
 		}
 			
