75c250fb12ad8d882e8eba497c1552e61540e5f4
somename <someemail>
2016-10-19 14:27:44 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class TestPlanetExplorer {
 
 	@Test
-	public void test_executeCommand() {
+	public void (expected=PlanetExplorerException.class) test_executeCommand() {
 		fail("Not yet implemented");
 	}
 }
