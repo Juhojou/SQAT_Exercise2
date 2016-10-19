07825d51a481b2cede3bdbe27224a77c91ff89c9
somename <someemail>
2016-10-19 14:41:12 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -14,7 +14,7 @@ public class TestPlanetExplorer {
 	@Test
 	public void test_explorerIsFacingNorth() throws PlanetExplorerException{
 		PlanetExplorer explorer = new PlanetExplorer(100, 100, null);
-		assertEquals("(0,0,N)", expoler.getStatus());
+		assertEquals("(0,0,N)", explorer.getStatus());
 	}
 
 	
