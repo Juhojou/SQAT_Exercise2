5958f226f7ed92a1e3a5c6a4e865b8670a286e9b
somename <someemail>
2016-10-19 16:22:48 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -5,15 +5,18 @@
 
 public class PlanetExplorer {
 	
-	private int xkoord = 0, ykoord = 0, ySize=0, xSize=0;
+	private int xkoord = 0, ykoord = 0, ySize=0, xSize=0, obstacleCount=0;
 	private String facingDirection = "N";
 	
 	public PlanetExplorer(int x, int y, String obstacles){
-		y = ySize;
-		x = xSize;
-		String[] obstaclesInGrid = obstacles.replaceAll("\\(", "").replaceAll("\\)","").replaceAll(",", " ").split(" ");
-		int xkoordObstacle = Integer.parseInt(obstaclesInGrid[0]);
-		int ykoordObstacle = Integer.parseInt(obstaclesInGrid[1]);
+		if (obstacles != null){
+			y = ySize;
+			x = xSize;
+			String[] obstaclesInGrid = obstacles.replaceAll("\\(", "").replaceAll("\\)","").replaceAll(",", " ").split(" ");
+			int xkoordObstacle = Integer.parseInt(obstaclesInGrid[0]);
+			int ykoordObstacle = Integer.parseInt(obstaclesInGrid[1]);
+			obstacleCount++;
+		}
 		
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
@@ -24,9 +27,8 @@ public class PlanetExplorer {
 	 */
 	}
 	
-	public Object getObstacleCount() {
-		
-		return null;
+	public int getObstacleCount() {
+		return obstacleCount;
 	}
 	
 	public int getPlanetYSize(){
