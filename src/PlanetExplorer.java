026d4a0b2297af3cafe1572a8ceeec52985dcd2d
somename <someemail>
2016-10-19 16:09:22 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -29,7 +29,7 @@ public class PlanetExplorer {
 		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
-
+		//For some reason I can't get e.g. "ff" work with my split
 		String[] commands = command.split("");
 		for (int j=0; j<command.length(); j++)
 		{
